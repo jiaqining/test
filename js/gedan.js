5a9796e7551188a7aceac1367732a3fe9e92f367
jiaqining <86393579+jiaqining@users.noreply.github.com>
2021-06-24 11:40:37 +0800

    Add files via upload

---
+++ js/gedan.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-containers', {
+      slidesPerView: 5,
+      spaceBetween: 5,
+      slidesPerGroup: 5,
+      loop: true,
+      loopFillGroupWithBlank: true,
+	  loopAdditionalSlides: 5, 
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	  autoplay: {
+	      delay: 3000,
+	      stopOnLastSlide: false,
+	      disableOnInteraction: true,
+	      }
+    });
---
+++ js/lunbo.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 2,
+      spaceBetween: 5,
+      slidesPerGroup: 2,
+      loop: true,
+      loopFillGroupWithBlank: true,
+	  loopAdditionalSlides: 2, 
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	  autoplay: {
+	      delay: 3000,
+	      stopOnLastSlide: false,
+	      disableOnInteraction: true,
+	      }
+    });
